1b1a9a3aa060255b58ec78eca953ccbb2bc827e2
benizmrn-dotcom <benizm0827@gmail.com>
2026-06-29 22:22:56 +0900

    done

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -18,11 +18,16 @@ resource "aws_db_instance" "this" {
   db_name  = var.db_name
   username = var.db_username
   password = "password"
-
-  skip_final_snapshot = true
-
   vpc_security_group_ids = [var.rds_sg_id]
   db_subnet_group_name   = aws_db_subnet_group.this.name
-
+  multi_az = true
   publicly_accessible = false
+  backup_retention_period = 7   
+  backup_window           = "03:00-04:00"  
+  maintenance_window      = "sun:04:00-sun:05:00" 
+  copy_tags_to_snapshot = true
+  skip_final_snapshot   = false
+  final_snapshot_identifier = "${var.env}-final-snapshot"
+  deletion_protection = true 
 }
+
