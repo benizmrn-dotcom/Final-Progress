a6be776eb7a1fcea585cea8b94a6f5db611c8130
benizmrn-dotcom <benizm0827@gmail.com>
2026-07-01 12:52:12 +0900

    Revert "allow HTTP access to ALB"
    This reverts commit a199c3541c2377da0689fa05b0a40242172a88fa.

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -7,8 +7,8 @@ resource "aws_security_group" "alb" {
   vpc_id = var.vpc_id
 
 ingress {
-  from_port   = 80
-  to_port     = 80
+  from_port   = 443
+  to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"] 
 }
